a841d55b837dac345942a8e2332d39245f99d577
HidekiHoshino <dondake@yandex.ru>
2017-10-30 18:30:00 +0300

    list conversion

--- tkinter/test.py
+++ tkinter/test.py
@@ -4,6 +4,7 @@ import yaml         # database editing
 
 
 class CreateRootWindow:
+    """Window Class."""
     def __init__(self, window):
         self.window = window
         # Title
@@ -12,11 +13,11 @@ class CreateRootWindow:
         # Creating stuff
         # Tabs frame
         self.tabs = tkinter.ttk.Notebook(window)
-		
+        
         # Entries tab & text label
         self.entries = tkinter.ttk.Frame(window)
         self.tabs.add(self.entries, text="Entries")
-		self.tabs.grid(row=0, column=0, sticky="nsew")
+        self.tabs.grid(row=0, column=0, sticky="nsew")
         
         # New entries tab & text label
         self.add = tkinter.ttk.Frame(self.tabs)
@@ -31,7 +32,7 @@ class CreateRootWindow:
         self.tree.heading("Title", text="Title")
         self.tree.heading("Theme", text="Theme")
         self.tree.heading("Podcast", text="Podcast")
-		self.tree.grid(row=0, column=0, sticky="nsew")
+        self.tree.grid(row=0, column=0, sticky="nsew")
         
         # The scrollbar
         self.scrollbar = tkinter.ttk.Scrollbar(
@@ -41,7 +42,7 @@ class CreateRootWindow:
             )
         # This feedbacks data to the scrollbar
         self.tree['yscrollcommand'] = self.scrollbar.set
-		self.scrollbar.grid(row=0, column=1, sticky="nes")
+        self.scrollbar.grid(row=0, column=1, sticky="nes")
         
         # Adding Labels and Entries
         self.new_entry_label = tkinter.ttk.Label(
@@ -78,8 +79,8 @@ class CreateRootWindow:
             text="Podcast:"
             )
         self.podcast_label.grid(row=3, column=0)
-		
-		# Podcast Entry.
+        
+        # Podcast Entry.
         self.podcast_entry = tkinter.ttk.Entry(self.add)
         self.podcast_entry.grid(row=3, column=1)
         
@@ -89,8 +90,8 @@ class CreateRootWindow:
             text="Theme:"
             )
         self.theme_label.grid(row=4, column=0)
-		
-	    # Theme Entry.
+        
+        # Theme Entry.
         self.theme_entry = tkinter.ttk.Entry(self.add)
         self.theme_entry.grid(row=4, column=1)
         
@@ -101,10 +102,7 @@ class CreateRootWindow:
             command=save_data)
         self.save_button.grid(row=5, column=0)
         
-    
-        
-		
-	    # This makes sure widgets stretch to the window
+        # This makes sure widgets stretch to the window
         window.grid_rowconfigure(0, weight=1)
         window.grid_columnconfigure(0, weight=1)
         self.entries.grid_rowconfigure(0, weight=1)
@@ -126,7 +124,7 @@ def show_data():
             )
 
 
-def save_data(data):
+def save_data():
     """Saves the data into the file."""
     with open("newdata.txt", "w", encoding="UTF-8") as outputfile:
         outputfile.write(yaml.dump_all(
@@ -137,7 +135,8 @@ def save_data(data):
         )
     rroot.save_button["text"] = "Saved."
 
-data = yaml.load_all(open("testdata.txt", "r", encoding="UTF-8"))
+data = list(yaml.load_all(open("testdata.txt", "r", encoding="UTF-8")))
+
 root = tkinter.Tk()
 rroot = CreateRootWindow(root)
 # Loads the data into the application.
