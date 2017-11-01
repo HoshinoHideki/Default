d7cc8d319c20cc771a41dad8d5f25ba6150f5928
HidekiHoshino <dondake@yandex.ru>
2017-11-01 09:00:24 +0300

    Some changes
    - Quotes
    - Context menu
    - Adding new Entries
    - New Functions for adding data and refresing the table

--- tkinter/test.py
+++ tkinter/test.py
@@ -1,12 +1,14 @@
-import tkinter      # interface
-import tkinter.ttk  # interface
-import yaml         # database editing
+import tkinter             # interface
+import tkinter.ttk         # 
+import tkinter.messagebox  # 
+import yaml                # database editing
 
 
 class CreateRootWindow:
     """Window Class."""
     def __init__(self, window):
         self.window = window
+        window.option_add('*tearOff', False)
         # Title
         window.title("Podcast Tracker")
         
@@ -37,11 +39,11 @@ class CreateRootWindow:
         # The scrollbar
         self.scrollbar = tkinter.ttk.Scrollbar(
             self.entries,
-            orient='vertical',
+            orient="vertical",
             command=self.tree.yview
             )
         # This feedbacks data to the scrollbar
-        self.tree['yscrollcommand'] = self.scrollbar.set
+        self.tree["yscrollcommand"] = self.scrollbar.set
         self.scrollbar.grid(row=0, column=1, sticky="nes")
         
         # Adding Labels and Entries
@@ -102,11 +104,31 @@ class CreateRootWindow:
             command=save_data)
         self.save_button.grid(row=5, column=0)
         
+        self.add_button = tkinter.ttk.Button(
+            self.add,
+            text="Add (test)",
+            command= lambda: add_data(self))
+        self.add_button.grid(row=5, column=1)
+        
+        # Right-click Menu
+        menu = tkinter.Menu(root)
+        menu.add_command(
+            label="Delete",
+            command = lambda:self.delete_entry()
+            )
+        root.bind('<3>', lambda e: menu.post(e.x_root, e.y_root))
+        
         # This makes sure widgets stretch to the window
         window.grid_rowconfigure(0, weight=1)
         window.grid_columnconfigure(0, weight=1)
         self.entries.grid_rowconfigure(0, weight=1)
         self.entries.grid_columnconfigure(0, weight=1)
+        
+    def delete_entry(self):
+        print(self.tree.item(
+            self.tree.selection())
+            )
+        self.tree.delete(self.tree.selection())
 
 
 def show_data():
@@ -114,19 +136,19 @@ def show_data():
     for item in data:
         rroot.tree.insert(
             "",
-            'end',
-            text=item[0]['Date'],
+            "end",
+            text=item["Date"],
             values=(
-                item[0]["Title"],
-                item[0]['Theme'],
-                item[0]["Podcast"]
+                item["Title"],
+                item["Theme"],
+                item["Podcast"]
                 )
             )
 
 
 def save_data():
     """Saves the data into the file."""
-    with open("newdata.txt", "w", encoding="UTF-8") as outputfile:
+    with open("testdata.txt", "w", encoding="UTF-8") as outputfile:
         outputfile.write(yaml.dump_all(
             data,
             allow_unicode=True,
@@ -134,12 +156,47 @@ def save_data():
             )
         )
     rroot.save_button["text"] = "Saved."
+    reload_data(rroot)
 
+
+def add_data(app):
+    """Adds new entry into the list."""
+    NewDate = app.date_entry.get()
+    NewTitle = app.title_entry.get()
+    NewPodcast = app.podcast_entry.get()
+    NewTheme = app.theme_entry.get()
+    NewEntry = {
+        "Date" : NewDate,
+        "Title" : NewTitle,
+        "Podcast" : NewPodcast,
+        "Theme" : NewTheme}
+    
+    # Make sure if every entry is filled
+    if not all(NewEntry.values()):
+        tkinter.messagebox.showinfo("Error", "Fill all fields.")
+    else:
+        data.append(NewEntry)
+        app.add_button["text"] = "Added."
+
+
+def clear_data(app):
+    app.tree.delete(*app.tree.get_children())
+
+
+def reload_data(app):
+    clear_data(app)
+    show_data()
+
+
+
+
+    
+# Loads the data into the application.
 data = list(yaml.load_all(open("testdata.txt", "r", encoding="UTF-8")))
+
 
 root = tkinter.Tk()
 rroot = CreateRootWindow(root)
-# Loads the data into the application.
 
 show_data()
 root.mainloop()
